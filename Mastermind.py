ce4f4fbd14c30fb90e91aa4b3af339fa4a7b72bf
rvvh <46648640+rvvh@users.noreply.github.com>
2020-05-25 21:42:22 +0200

    25MAY2020

--- Mastermind.py
+++ Mastermind.py
@@ -1,77 +1,99 @@
-# Mastermind
-# 6 options in a 4 size code allowing for duplicates
+# Mastermind 25MAY20
+# 6 options in a 4 size code and allowing for duplicates (1296 options)
+# This routine starts with choice [1,1,2,2] and removes all solutions from s that do not have the same number of black and white
+# It will randomly pick a remaining option from s and repeat till only one option, the solution, remains.
 
 from random import choice
+import time
 
-#Fill array with all possible solution at the start of a game
-#Returns an array with all possible solution 
-def all_options():
+# Fill array s with all possible solution at the start of the game
+# Returns an array with all possible solutions s
+def solutions():
+    s=[]
     for i in range (1,7):
         for j in range (1,7):
             for k in range(1,7):
                 for l in range (1,7):
-                    options.append([i,j,k,l])
-    return (options)
+                    s.append([i,j,k,l])
+    return (s)
 
-# Checking the attempt against the code
+# Checking the guess against the secret
 # Returns the amount of Black's and White's
-def check(attempt,code):
+def check(guess, secret):
     black=0
     white=0
-    attempt_count=[0,0,0,0,0,0]
-    code_count   =[0,0,0,0,0,0]
+    secret_count=[0,0,0,0,0,0]
+    guess_count =[0,0,0,0,0,0]
 # count the number frequency
-    attempt = list(map(int, attempt))
-    code    = list(map(int, code))
-    
     for i in range(4):
-        code_count   [code[i]-1]+=1
-        attempt_count[attempt[i]-1]+=1
-
-# count the black's and white's
+        secret_count [secret[i]-1]+=1
+        guess_count  [guess [i]-1]+=1
+# count the black's and remove them so they don't count as white's 
     for i in range (4):
-        if code[i]==attempt[i]:
+        if  secret[i] == guess[i]:
             black +=1
-            code_count[attempt[i]-1]-=1
+            secret_count [secret[i]-1]-=1
+            guess_count  [guess [i]-1]-=1  
+# count the whites    
     for i in range (6):
-            white += min (attempt_count[i-1], code_count[i-1])
+            white += min (guess_count[i], secret_count[i])
     return (black, white)
 
 # Removes all impossible solutions from solutions that do not provide the same result in Black's and White's
 # Returns an array with the remaining possible solutions
-def eliminate (attempt, options, black, white):
+def eliminate (guess, s, black, white):
     opts=[]
-    count =[]
-    for i in range (len(options)):
-        bl,wh=check (attempt, options[i])
-        if (bl==black and wh==white):
-            opts.append(options[i])
+    for i in range (len(s)):
+        b,w=check (guess, s[i])
+        if (b==black and w==white):
+            opts.append(s[i])
     return (opts)
 
 # Start
 
 # Create all possible options
-options = []
-options=all_options()
+s = solutions()
+s_without_guesses=s.copy()
 
-# Pick a random code from the possible options
-code = choice (options)
+# Pick a random secret from the possible solutions
+secret = choice (s)
 
-print ("Code:",code)
+print ("Code:  ",secret)
 
-# Define a first attempt
-attempt = [1,1,2,2]
+# Define a first guess
+guess   = [1,1,2,2]
+guesses = 0
 
-n=0
-while len(options)>1:
-    n+=1
+while len(s)>1:
+    guesses+=1
+    b=0
+    w=0
+    black, white = check (guess, secret)
+    print ("Guess",guesses, guess, "Black:", black, "White:", white, "Secret codes remaining:",len(s))
+
+    s_without_guesses.remove(guess)
+        
+    s = eliminate (guess, s, black, white)
     
-    black, white= check (attempt, code)
-    print ("Attempt",n, attempt, "Black:", black, "White:", white)
-    
-    options = eliminate (attempt, options, black, white)
-    print ("Remaining options:",len(options))
+    guess = choice (s)
+print ("Secret:", guess,"found in",guesses+1,"guesses")
 
-    attempt = choice (options)
-    
-print ("Attempt",n+1, attempt, "Black: 4", "White: 0")
+# apply minimax routine to choose the next best option. 
+# This might even pick a guess from the full set of options, knowing it does not belong to the possible solutions, to assure a better result overall.
+# but this doesnt work yet, getting close though
+
+'''
+    t=[]
+    for i in range (len(s)):
+        for j in range (len(s_without_guesses)):
+            black, white = check (s[i],s_without_guesses[j])
+            o=[]
+            c=0
+            #check all possible b and w results
+            for k in range (5):
+                for m in range (b,5):
+                    b,w = eliminate (s[i],s_without_guesses[j], k, m)
+                    if (b==black and w==white):
+                        c+=1
+            t[i]+=c
+'''
